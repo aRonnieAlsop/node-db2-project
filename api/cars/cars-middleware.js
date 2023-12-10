4bbd3f98845e00804c1012c7f3efeb48aa060d9e
Arron Alsop <alsop.arron@gmail.com>
2023-12-09 19:44:14 -0800

    'wip'

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,18 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+
+const db = require('../../data/db-config')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car) {
+      next({ status: 404, message: `car with id ${req.params.id} is not found` })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -10,6 +23,24 @@ const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const existingVin = await db('cars').where('name', req.body.vin.trim()).first()
+    if (existingVin) {
+      next({ status: 400, message: `vin ${req.body.vin} already exists` })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
+
+
+
+
+//   - `checkCarPayload` returns a status 400 with a `{ message: "<field name> is missing" }` if any required field is missing.
+
+//   - `checkVinNumberValid` returns a status 400 with a `{ message: "vin <vin number> is invalid" }` if the vin number is [invalid](https://www.npmjs.com/package/vin-validator).
+
+//   - `checkVinNumberUnique` returns a status 400 with a `{ message: "vin <vin number> already exists" }` if the vin number already exists in the database.
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '12345678910111213',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 30000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '12345678910155555',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 250000,
+        title: 'fair',
+        transmission: 'manual'
+    },
+    {
+        vin: '12345678910666666',
+        make: 'ford',
+        model: 'focus',
+        mileage: 30000,
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
