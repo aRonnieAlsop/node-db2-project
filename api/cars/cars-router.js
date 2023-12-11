3d28825cb1e5b35cc1decca5479cd5ab76502035
Arron Alsop <alsop.arron@gmail.com>
2023-12-11 06:41:34 -0800

    'wip'

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -3,7 +3,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+    try {
+        const cares = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get('/:id', async (req, res, next) => {
